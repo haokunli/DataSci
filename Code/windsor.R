2ecc5994f06ed49755a09f709752bb99a048b902
cinnData <canela.miguel@icloud.com>
2017-01-04 16:47:00 +0100

    Add files via upload

---
+++ Code/windsor.R
@@ -0,0 +1,37 @@
+## Windsor housing prices ##
+
+# Setting work directory (customize) #
+setwd("/Users/miguel/Dropbox (Personal)/Current jobs/DATA-2017-1/DATA-02")
+
+# Reading data
+windsor <- read.csv(file="windsor.csv")
+
+# Checks #
+dim(windsor)
+head(windsor)
+tail(windsor)
+str(windsor)
+
+# Linear regression model #
+fm1 <- price ~ lotsize + nbdrm + nbhrm + nstor + ngar + drive + recrm +
+  base + gas + air + neigh  
+mod1 <- lm(formula=fm1, data=windsor) 
+
+# Exploring the model #
+names(mod)
+summary(mod)
+
+# Statistical evaluation of the model #
+pred <- predict(mod, newdata=windsor)
+res <- windsor$price - pred
+sd(res)
+sd(windsor$price)
+sd(res)/sd(windsor$price)
+
+# Evaluation in dollar terms #
+mean(abs(res)>40000)
+
+# Figure #
+par(mfrow=c(1,2))
+hist(windsor$price, main="", xlab="Price")
+hist(res, main="", xlab="Prediction error")
