dbed21673287cc0bdb23e457c9ef28d2bf650f80
cinnData <canela.miguel@icloud.com>
2017-09-20 18:07:02 +0200

    Update king.R

--- Code/king.R
+++ Code/king.R
@@ -32,31 +32,3 @@ plot(king$price ~ pred_price, pch=20, main="Figure 2a. Actual vs predicted price
  xlab="Predicted price", ylab="Actual price")
 plot(pred_error ~ pred_price, pch=20, main="Figure 2b. Prediction error vs predicted price",
   xlab="Predicted price", ylab="Prediction error")
-
-
-
-#Logarithmic scale #
-fm_log <- log(price) ~ bedrooms + bathrooms + sqft_living + sqft_lot + floors +
-  waterfront + condition + sqft_above + 
-  yr_built + yr_renovated + lat + long
-model_log <- lm(formula=fm_log, data=king)
-summary(model_log)
-pred_price_log <- exp(predict(model_log, newdata=king))
-hist(pred_price_log  - king$price)
-
-# Including zipcode #
-fm2 <- price ~ bedrooms + bathrooms + sqft_living + sqft_lot + floors +
-  waterfront + condition + sqft_above + 
-  yr_built + yr_renovated + lat + long + substr(zipcode, 1, 4)
-model2<- lm(formula=fm2, data=king)
-summary(model2)
-
-# Random forest #
-library(randomForest)
-N <- floor(nrow(king)/2)
-train <- sample(1:N, nrow(king)/2,  replace=F)
-mod6 <- randomForest(formula=fm, data=king[train,], ntree=500, nodesize=0.01*N, importance=T) 
-pred6_train <- predict(mod6, newdata=king[train,])
-pred6_test <- predict(mod6, newdata=king[-train,])
-cor(pred6_train, king$price[train])
-cor(pred6_test, king$price[-train])
