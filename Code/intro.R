000b6c9dc766c116d0b2d4709e56b6fde0cd82c9
cinnData <canela.miguel@icloud.com>
2017-09-20 16:15:25 +0200

    Update intro.R

--- Code/intro.R
+++ Code/intro.R
@@ -21,27 +21,14 @@ z
 y_factor <- factor(y)
 y_factor
 
-# Matrix #
-A <- matrix(1:24, nrow=4)
-A
-A[2,3]
-
 # List #
 L <- list(L1=1:10, L2=c("Messi", "Neymar", "Cristiano"))
 L[[1]]
 L$L2
 
-# Data frame #
-df <- data.frame(v1=1:10, v2=10:1, v3=rep(-1,10))
-df
-df$v1
-
 # Subsetting
 x[1:3]
 x[x>=5]
-A[1:2, 3:6]
-df[, -3]
-df[df$v1<df$v2, ]
 
 # Function
 f <- function(x) 1/(1-x^2)
