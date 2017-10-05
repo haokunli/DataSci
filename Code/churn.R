193ef0165b7128d2851752b2752da97ee88a1f4d
cinnData <canela.miguel@icloud.com>
2017-10-05 08:44:08 +0200

    Update churn.R

--- Code/churn.R
+++ Code/churn.R
@@ -1,19 +1,16 @@
 ## The churn model ##
 
-# Setting work directory #
-setwd("/Users/miguel/Dropbox (IESE)/Data archive/DATA")
-
-# Reading data #
-churn <- read.csv("churn.csv")
+# Reading data (customize) #
+churn <- read.csv("churn.csv", stringsAsFactors=F)
 str(churn)
 N <- nrow(churn)
 
 # Formula #
-fm = churn ~ aclength + intplan + dataplan + ommin + omcall + otmin +
-  otcall + ngmin + ngcall + imin + icall + cuscall
+fm = churn ~ aclength + intplan + dataplan + ommin + omcall +
+  otmin + otcall + ngmin + ngcall + imin + icall + cuscall
 
 # Logistic regression #
-mod <- glm(fm, data=churn, family="binomial") 
+mod <- glm(formula=fm, data=churn, family="binomial")
 summary(mod)
 
 # Inspecting datagb #
@@ -33,16 +30,16 @@ tp1
 fp1 <- conf1["TRUE", "FALSE"]/sum(conf1[,"FALSE"])
 fp1
 
-# Splitting the histogram
+# Histograms #
 par(mfrow=c(1,2))
-hist(pred1[churn$churn==1], breaks=20, main="Figure 3a. Predictive scores (churners)",
-  xlab="Predictive scores")
-hist(pred1[churn$churn==0], breaks=20, main="Figure 3b. Predictive scores (non-churners)",
-  xlab="Predictive scores")
+hist(pred1[churn$churn==1], breaks=20,
+  main="Figure 2a. Scores (churners)", xlab="Churn score")
+hist(pred1[churn$churn==0], breaks=20,
+  main="Figure 2b. Scores (non-churners)", xlab="Churn score")
 
 # Lower cutoff
 cut2 <- mean(churn$churn)
-conf2 <- table(pred1 > cut2, churn$churn==1)
+conf2 <- table(pred1 > cut2, churn$churn == 1)
 conf2
 tp2 <- conf2["TRUE", "TRUE"]/sum(conf2[,"TRUE"])
 tp2
